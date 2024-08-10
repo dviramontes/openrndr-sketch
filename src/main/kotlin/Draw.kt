eae5ce8e4cd9ba32d0a9afef22ac260116c72a4d
David Viramontes <dviramontes@gmail.com>
2024-08-10 13:28:33 -0400

    draw with polar

--- src/main/kotlin/Draw.kt
+++ src/main/kotlin/Draw.kt
@@ -1,5 +1,6 @@
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
+import org.openrndr.math.Polar
 import org.openrndr.math.Vector2
 import kotlin.math.cos
 import kotlin.math.sin
@@ -13,7 +14,8 @@ fun main() = application {
         extend {
             val mainRadius = 100.0
             val radius = 12.0
-            val subRect = drawer.bounds.sub(0.0, 0.0, 0.5, 1.0)
+//            val subRect = drawer.bounds.sub(0.0, 0.0, 0.5, 1.0)
+            val subRect = drawer.bounds
 
             drawer.clear(ColorRGBa.CYAN)
             drawer.rectangle(subRect)
@@ -21,11 +23,8 @@ fun main() = application {
             drawer.circle(subRect.center, mainRadius)
 
             repeat(10) {
-                val a = it * 2 * Math.PI / 10
-                val p = subRect.center + Vector2(
-                    cos(a) * mainRadius,
-                    sin(a) * mainRadius
-                )
+                val a = 360.0 * it / 10
+                val p = subRect.center + Polar(a, mainRadius).cartesian
                 drawer.circle(p, radius)
             }
         }
